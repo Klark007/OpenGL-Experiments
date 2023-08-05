a9ef3630ee83f51df9129c91cb110044963a41eb
Klark007 <jan.grunder@yahoo.de>
2023-08-05 17:22:00 +0200

    Controllable camera

--- OpenGL Experiments/src/main.cpp
+++ OpenGL Experiments/src/main.cpp
@@ -14,11 +14,19 @@
 #include "Camera.h"
 
 #include <iostream>
-#include <direct.h> ^// windows get cwd
+#include <direct.h> // windows get cwd
 
 void glfw_error_callback(int error, const char* description);
 void glfw_framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void glfm_mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+void handle_input(GLFWwindow* window);
+
 unsigned int loadTexture(const char* path);
+
+int screen_x = 800;
+int screen_y = 600;
+
+Camera camera = { glm::vec3(0.0, 2.0, 4.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0) };
 
 int main()
 {
@@ -59,6 +67,15 @@ int main()
 	}
 
 	glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
+
+	// disable cursor so can use raw mouse movement
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	if (!glfwRawMouseMotionSupported()) {
+		std::cerr << "Raw mouse motion not supported" << std::endl;
+		return -1; // could fall back on just cursor position
+	}
+
+	glfwSetCursorPosCallback(window, glfm_mouse_move_callback);
 
 	glfwSwapInterval(1); // default swap interval is 0 which can lead to screen tearing
 
@@ -158,14 +175,14 @@ int main()
 	// shaders
 	
 	Shader vertex_shader{ GL_VERTEX_SHADER };
-	vertex_shader.add_source_from_file("./shaders/phong.vs");
+	vertex_shader.add_source_from_file("shaders/phong.vs");
 	std::cout << "Vertex shader compilation" << std::endl;
 	vertex_shader.compile();
 	vertex_shader.print_compile_error();
 
 	Shader fragment_shader{ GL_FRAGMENT_SHADER };
 	std::cout << "Fragment shader compilation" << std::endl;
-	fragment_shader.add_source_from_file("./shaders/phong.fs");
+	fragment_shader.add_source_from_file("shaders/phong.fs");
 	fragment_shader.compile();
 	fragment_shader.print_compile_error();
 
@@ -178,7 +195,6 @@ int main()
 	glm::mat4 model = glm::mat4(1.0);
 	//model = glm::rotate(model, glm::radians(45.0f), glm::normalize(glm::vec3(0.0, 0.0, 1.0)));
 
-	Camera camera = {glm::vec3(0.0, 2.0, 4.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0)};
 	glm::mat4 view = camera.generate_view_mat();
 
 	glm::mat4 projection;
@@ -195,7 +211,7 @@ int main()
 
 	program.set1f("mat.shininess", 64.0);
 
-	program.set_vec3f("light.diffuse", 0.5f, 0.5f, 0.5f);
+	program.set_vec3f("light.diffuse", 0.65f, 0.65f, 0.65f);
 	program.set_vec3f("light.specular", 1.0, 1.0, 1.0);
 	program.set_vec3f("light.pos", 1.2, 1.0, 2.0);
 
@@ -204,7 +220,7 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
-
+		handle_input(window);
 		
 		// rendering
 		glClearColor(0.25, 0.5, 0.1, 1);
@@ -220,11 +236,10 @@ int main()
 
 		program.set_mat4f("model", model);
 
-		glm::vec3 camera_pos = glm::vec3(5 * std::cos(glfwGetTime()), -2.0, 5 * std::sin(glfwGetTime()));
-		camera.set_pos(camera_pos);
 		view = camera.generate_view_mat();
 		program.set_mat4f("view", view);
 
+		glm::vec3 camera_pos = camera.get_pos();
 		program.set_vec3f("camera_pos", camera_pos);
 
 		// drawArray is for without indices, drawElements for indexed drawing
@@ -294,4 +309,60 @@ void glfw_error_callback(int error, const char* description)
 
 void glfw_framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
+	screen_x = width;
+	screen_y = height;
+}
+
+void glfm_mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	const double strength = 0.001;
+	static double xlast = screen_x / 2;
+	static double ylast = screen_y / 2;
+	
+	double dx = xpos - xlast;
+	double dy = ypos - ylast;
+
+	glm::vec3 dir = camera.get_dir();
+
+	// not efficient, could compute once and update
+	double yaw   = atan2(dir.z, dir.x);
+	double pitch = asin(dir.y);
+
+	yaw   += dx * strength;
+	pitch += -dy * strength;
+
+	dir.x = (float) (cos(yaw) * cos(pitch));
+	dir.y = (float) sin(pitch);
+	dir.z = (float) (sin(yaw) * cos(pitch));
+
+	camera.set_dir(dir);
+
+	xlast = xpos;
+	ylast = ypos;
+}
+
+void handle_input(GLFWwindow* window)
+{
+	const float strength = 0.1f;
+	
+	float dx = 0.0f;
+	float dy = 0.0f;
+
+	if (glfwGetKey(window, GLFW_KEY_W))
+		dx += 1;
+	if (glfwGetKey(window, GLFW_KEY_S))
+		dx -= 1;
+	if (glfwGetKey(window, GLFW_KEY_D))
+		dy += 1;
+	if (glfwGetKey(window, GLFW_KEY_A))
+		dy -= 1;
+
+	glm::vec3 pos = camera.get_pos();
+	glm::vec3 dir = camera.get_dir();
+
+	pos += dx * strength * dir;
+	glm::vec3 side = glm::cross(dir, camera.get_up()); // can assume both normalized
+	pos += dy * strength * side;
+
+	camera.set_pos(pos);
 }
