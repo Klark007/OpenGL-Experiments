657a96aac1e335c81d7ff7d2264b462938aaeebe
Klark007 <jan.grunder@yahoo.de>
2024-01-11 12:35:52 +0100

    Parallel Perlin3D

--- OpenGL Experiments/src/noise/Perlin3D.cpp
+++ OpenGL Experiments/src/noise/Perlin3D.cpp
@@ -3,6 +3,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/noise.hpp>
 
+#include <numeric> // for iota
+#include <execution> // for parallel for_each
+
 template<class T>
 inline Perlin3D<T>::Perlin3D(uint res_x, uint res_y, uint res_z, uint scale_x, uint scale_y, uint scale_z)
   : Noise<T>(res_x, res_y, res_z, 1), scale_x{ scale_x }, scale_y{ scale_y }, scale_z { scale_y }
@@ -14,10 +17,35 @@ inline Perlin3D<T>::Perlin3D(uint res_x, uint res_y, uint res_z, uint scale_x, u
 template<class T>
 void Perlin3D<T>::gen_img()
 {
-  glm::vec3 position;
+  auto image_lambda = [&](uint z) {
+    glm::vec3 position;
+    const glm::vec3 tiling = glm::vec3(scale_x, scale_y, scale_z);
 
-  const glm::vec3 tiling = glm::vec3(scale_x, scale_y, scale_z);
+    for (uint y = 0; y < this->res_y; y++) {
+      for (uint x = 0; x < this->res_x; x++) {
+        position = glm::vec3(
+          (float)x / this->res_x * scale_x,
+          (float)y / this->res_y * scale_y,
+          (float)z / this->res_z * scale_z
+        );
+        float perlin = (glm::perlin(position + offset, tiling) + 1.0) / 2;
+        T value = this->distance_to_val(perlin);
+        this->img_data.at(this->idx(x, y, z, 0)) = value;
+      }
+    }
+  };
 
+  std::vector<uint> z_range = std::vector<uint>(this->res_z);
+  std::iota(z_range.begin(), z_range.end(), 0);
+
+  std::for_each(
+    std::execution::par,
+    z_range.begin(),
+    z_range.end(),
+    image_lambda
+  );
+
+  /*
   for (uint z = 0; z < this->res_z; z++) {
     for (uint y = 0; y < this->res_y; y++) {
       for (uint x = 0; x < this->res_x; x++) {
@@ -32,6 +60,8 @@ void Perlin3D<T>::gen_img()
       }
     }
   }
+  */
+  
 }
 
 template class Perlin3D<unsigned char>;
